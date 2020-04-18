cc8b0742f070328e8e57cd6e9b3bfb0b354735b4
Felipe Fadoni <felipe.fadoni@gsw.com.br>
2020-04-18 17:58:33 -0300

    first commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRoutes = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRoutes.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRoutes.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parseDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      provider,
+      date: parseDate,
+    });
+
+    return response.json(appointment);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export default appointmentsRoutes;
